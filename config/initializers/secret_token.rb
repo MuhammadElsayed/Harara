365bd3a8b8c7dc538a232641b1da07cf6c96a98a
MuhammadElsayed <muhammad.elsayed89@gmail.com>
2012-03-27 09:06:36 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Harara::Application.config.secret_token = '077531573cae47c014fc5a6f0093832e3f5a6596f3e9762a0a5029cd2f217db061c930d4ea5f47d1aec0a2c988b3d6643207c044e5d133713db033b0fb89a342'
